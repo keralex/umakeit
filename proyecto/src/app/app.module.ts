d3b28379dd86db53904c89dda909cc10c940bb2d
unknown <kerlis.aguado@correo.unimet.edu.ve>
2018-09-30 15:25:11 -0600

    merge

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -2,23 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { FooterComponent } from './footer/footer.component';
-import { RouterModule, Routes } from '@angular/router';
-import { RegistroComponent } from './registro/registro.component';
-const rutas: Routes = [
-  { path: '', component: InicioComponent},
-  { path: 'registro', component: RegistroComponent }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    InicioComponent,
-    FooterComponent,
-    RegistroComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
