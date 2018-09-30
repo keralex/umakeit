7bcdb1749eb07d9c62562a42329b4c84214cae9e
unknown <kerlis.aguado@correo.unimet.edu.ve>
2018-09-30 15:43:07 -0600

    iniciosesion

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
+import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    FooterComponent,
+    IniciosesionComponent,
+    NavbarComponent
+
   ],
   imports: [
     BrowserModule
