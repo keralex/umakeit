421bd5a0bf698cc2fe354fedf12c8c140520db60
keralex <kerlis.aguado@correo.unimet.edu.ve>
2018-10-02 07:36:21 -0600

    nueva

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { IniciosesionComponent } from './Components/iniciosesion/iniciosesion.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    FooterComponent,
+    IniciosesionComponent,
+    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
