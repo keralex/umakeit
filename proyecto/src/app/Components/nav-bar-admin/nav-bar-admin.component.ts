e1fe20f28f7019d2a8194409daf795d23ae4b366
mike160300 <miguel.jaimes@correo.unimet.edu.ve>
2018-11-14 23:28:50 -0400

    Falto este por navadmin

--- proyecto/src/app/Components/nav-bar-admin/nav-bar-admin.component.ts
+++ proyecto/src/app/Components/nav-bar-admin/nav-bar-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth-service.service';
+import { FirestoreService } from 'src/app/firestore.service';
 
 @Component({
   selector: 'app-nav-bar-admin',
@@ -7,8 +8,26 @@ import { AuthService } from 'src/app/auth-service.service';
   styleUrls: ['./nav-bar-admin.component.css']
 })
 export class NavBarAdminComponent implements OnInit {
+  usuarioiout;
+  uss=[];
+  name:string;
 
-  constructor(private fireout: AuthService) { }
+  constructor(private fireout: AuthService, private firestoreservice:FirestoreService) { 
+    this.usuarioiout=fireout.getU();
+    this.uss=firestoreservice.au;
+    this.name=this.getname();
+  }
+
+   
+  getname(){
+    for(var i=0;i<this.uss.length;i++){
+      if(this.uss[i].id==this.usuarioiout){
+        return this.uss[i].name;
+      }
+    }
+  }
+
+
 
   ngOnInit() {
   }
