071ed1d48f993bfa6004eeedcc9ee26ca3776013
keralex <kerlis.aguado@correo.unimet.edu.ve>
2018-11-27 07:35:09 -0600

    intento de usuario

--- proyecto/src/app/Components/perfilusuario/perfilusuario.component.ts
+++ proyecto/src/app/Components/perfilusuario/perfilusuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../auth-service.service';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-perfilusuario',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfilusuario.component.css']
 })
 export class PerfilusuarioComponent implements OnInit {
+  usuario;
 
-  constructor() { }
+  constructor(public db:AngularFirestore , public authService:AuthService) {
+     this.db.collection('usuarios').doc(this.authService.getId()).ref.get().then(doc=> {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        console.log(doc.data().name);
+        this.usuario=doc.data();
+      }
+    }
+
+    ).catch(err => {
+      console.log('Error getting document', err);
+    });
+    console.log(usuario);
+   }
 
   ngOnInit() {
+    
   }
 
 }
